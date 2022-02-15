1d220b316ed6cf61198b6f5714aa79f04fb7235f
00-01 <59557934+00-01@users.noreply.github.com>
2022-02-16 00:23:45 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,34 @@
+import numpy as np
+
+
+
+img = [[[ 0  0  0]
+        [ 0  1  0 ]
+        [ 0  0  0]]
+         
+       [[ 0  0  0]
+        [ 0  1  0 ]
+        [ 0  0  0]]
+         
+       [[ 0  0   0]
+        [ 0  1  0]
+        [ 0  0  0]]]
+
+segment = []
+first = 1
+num = 1
+around_g = ###
+new_num = ###
+for i in img:    # RGB
+    for m in i:    # line
+        for g in m:    # pixel
+            if first == num:    # g = 1 if first pixel
+                first = 0
+                segment.append(num)
+            else:
+                if {g close to {around_g}}:    # g = 
+                    g = {around_g}
+                    segment.append(g)
+                else:
+                    segment.append(g)
+  
